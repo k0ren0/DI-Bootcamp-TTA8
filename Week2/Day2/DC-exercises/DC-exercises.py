3d6bc494249d45337ba9f764aee44d759b0f104e
Viktor <korneevv@gmail.com>
2023-11-28 23:06:43 +0200

    w2d3ex-XP-ex-DC-final

--- Week2/Day2/DC-exercises/DC-exercises.py
+++ Week2/Day2/DC-exercises/DC-exercises.py
@@ -4,78 +4,30 @@
     # Challenge 1
     # ----------------------------------------------------------------
 
+# # Input
+# user_number = int(input("Enter a number: "))
+# user_length = int(input("Enter the desired length: "))
 
-# #Input
-# user_word = input("Enter word: ")
-
-# index_dict = {}
-
-# # dictionary
-# for idx, letter in enumerate(user_word):
-#     if letter not in index_dict:
-#         index_dict[letter] = [idx]
-#     else:
-#         index_dict[letter].append(idx)
-
-# # Result
-# print(index_dict)
+# # Print the list
+# multiples_list = [user_number * i for i in range(1, user_length + 1)]
+# print(multiples_list)
 
     # ----------------------------------------------------------------
     # Challenge 2
     # ----------------------------------------------------------------
 
+# # Input info
+# user_input = input("Enter a word: ")
 
-# items_purchase1 = {
-#     "Water": "$1",
-#     "Bread": "$3",
-#     "TV": "$1,000",
-#     "Fertilizer": "$20"
-# }
-# wallet1 = "$300"
-# affordable_items_list1 = []
+# # Remove consecutive duplicates
+# result_str = ""
+# prev_char = None
 
-# for item, price in sorted(items_purchase1.items()):
-#     item_price = int(price.replace("$", "").replace(",", ""))
-#     if item_price <= int(wallet1.replace("$", "")):
-#         affordable_items_list1.append(item)
+# for char in user_input:
+#     if char != prev_char:
+#         result_str += char
+#     prev_char = char
 
-# result1 = sorted(affordable_items_list1) if affordable_items_list1 else "Nothing"
-# print(result1)
-
-# items_purchase2 = {
-#     "Apple": "$4",
-#     "Honey": "$3",
-#     "Fan": "$14",
-#     "Bananas": "$4",
-#     "Pan": "$100",
-#     "Spoon": "$2"
-# }
-# wallet2 = "$100"
-# affordable_items_list2 = []
-
-# for item, price in sorted(items_purchase2.items()):
-#     item_price = int(price.replace("$", "").replace(",", ""))
-#     if item_price <= int(wallet2.replace("$", "")):
-#         affordable_items_list2.append(item)
-
-# result2 = sorted(affordable_items_list2) if affordable_items_list2 else "Nothing"
-# print(result2)
-
-# items_purchase3 = {
-#     "Phone": "$999",
-#     "Speakers": "$300",
-#     "Laptop": "$5,000",
-#     "PC": "$1200"
-# }
-# wallet3 = "$1"
-# affordable_items_list3 = []
-
-# for item, price in sorted(items_purchase3.items()):
-#     item_price = int(price.replace("$", "").replace(",", ""))
-#     if item_price <= int(wallet3.replace("$", "")):
-#         affordable_items_list3.append(item)
-
-# result3 = sorted(affordable_items_list3) if affordable_items_list3 else "Nothing"
-# print(result3)
-
-    # END..........
+# # Result
+# print("Original word:", user_input)
+# print("Word with consecutive duplicates removed:", result_str)
