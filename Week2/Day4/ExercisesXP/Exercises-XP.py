469b1ce3a6888294493a15249536c5813f84a9c5
Viktor <korneevv@gmail.com>
2023-11-29 16:07:36 +0200

    w2d4ex-XP-5

--- Week2/Day4/ExercisesXP/Exercises-XP.py
+++ Week2/Day4/ExercisesXP/Exercises-XP.py
@@ -12,17 +12,62 @@
     # Exercise 2: What’s Your Favorite Book ?
     # ----------------------------------------------------------------
 
-title = input("One of my favorite books is: ")
+# title = input("One of my favorite books is: ")
 
-def favorite_book(title):
-    return title
+# def favorite_book(title):
+#     return title
 
-title_customer = favorite_book(title)
-print(f"One of my favorite books is {title_customer}")
+# title_customer = favorite_book(title)
+# print(f"One of my favorite books is {title_customer}")
 
     # ----------------------------------------------------------------
-    # Exercise 2: What’s Your Favorite Book ?
+    # Exercise 3 : Some Geography
     # ----------------------------------------------------------------
+
+
+# def describe_city(city, country):
+#     print(city + " is in " + country)
+#     return city, country
+
+# describe_city("Minsk", "Belarus")
+
+
+    # ----------------------------------------------------------------
+    # Exercise 4 : Random
+    # ----------------------------------------------------------------
+
+# import random
+
+# title = int(input("Input your number 1-100: "))
+# def randomizer ():
+#     gen_num = random.randint(1,101) 
+#     if title == gen_num:
+#         print(f"You are lucky. Compare {title}")
+#         print("\n")
+#     else: 
+#         print(f"You'll be lucky next time! Numbers were: {title} and {gen_num}")
+
+#     return title, gen_num
+
+# randomizer()
+
+
+#     # ----------------------------------------------------------------
+#     # Exercise 5 : Let’s Create Some Personalized Shirts !
+#     # ----------------------------------------------------------------
+
+# def make_shirt(size, text):
+#     print(f'The size of the shirt is {size} and the text is "{text}"')
+
+# make_shirt(38, "Nice size")
+# make_shirt(size="Large", text="I love Python")
+
+# make_shirt(size="Large", text="Don't buy it")
+# make_shirt(size="medium", text="Don't buy it")
+# make_shirt(size="small", text="Don't buy it")
+
+
+
 
 
 
