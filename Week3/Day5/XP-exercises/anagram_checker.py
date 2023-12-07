bff46e023b05dee3476f7b9422f66dcced2d4b32
Viktor <korneevv@gmail.com>
2023-12-07 17:17:19 +0200

    w3d5_XP_miniproject_done

--- Week3/Day5/XP-exercises/anagram_checker.py
+++ Week3/Day5/XP-exercises/anagram_checker.py
@@ -1,24 +1,15 @@
-
-
 class AnagramChecker:
-    def __init__(self, text) -> None:
-        self.text = text
+    def __init__(self, words):
+        self.words = words
+        with open(words, 'r') as file:
+            self.words = set(word.strip().lower() for word in file)
 
-    @classmethod
-    def from_file(cls, file_path):
-        with open(file_path, 'r', encoding='utf-8') as file:
-            file_text = file.read()
-        return cls(file_text)
-    
+    def is_valid_word(self, word):
+        return word.lower() in self.words
 
+    def get_anagrams(self, word):
+        word = word.lower()
+        return [w for w in self.words if self.is_anagram(word, w) and word != w]
 
-    def main():
-        print("Hello")
-
-        file_path = 'sowpods.txt' 
-        text = from_file(file_path)
-
-
-
-    # if __name__ == "__main__":
-#     main()
+    def is_anagram(self, word1, word2):
+        return sorted(word1.lower()) == sorted(word2.lower())
