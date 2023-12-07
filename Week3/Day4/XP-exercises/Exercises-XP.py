5cd42294ea4b08836d8c5250e28c630c6dbdac19
Viktor <korneevv@gmail.com>
2023-12-07 07:47:04 +0200

    w3d4_ex_all

--- Week3/Day4/XP-exercises/Exercises-XP.py
+++ Week3/Day4/XP-exercises/Exercises-XP.py
@@ -3,48 +3,42 @@
 # #Exercise 1 – Random Sentence Generator
 # #----------------------------------------------------------------
 
-import random
+# import random
 
-def get_words_from_file(file_path):
-    with open(file_path, encoding = 'utf-8', mode='r+') as file:
-        words = file.read().split()
-    return words
+# def get_words_from_file(file_path):
+#     with open(file_path, encoding = 'utf-8', mode='r+') as file:
+#         words = file.read().split()
+#     return words
 
-def get_random_sentence(length, words):
-    if length > len(words):
-        print("Warning: Requested sentence length is greater than the number of words in the list.")
-        length = len(words)
+# def get_random_sentence(length, words):
+#     if length > len(words):
+#         print("Warning: Requested sentence length is greater than the number of words in the list.")
+#         length = len(words)
 
-    random_words = random.sample(words, length)
-    sentence = ' '.join(random_words)
-    return sentence.lower()
+#     random_words = random.sample(words, length)
+#     sentence = ' '.join(random_words)
+#     return sentence.lower()
 
-def main():
-    print("Welcome to the Random Sentence Generator!")
-    print("This program will create a random sentence for you.")
+# def main():
+#     print("Welcome to the Random Sentence Generator!")
 
-    # Get words from the file
-    file_path = 'sowpods.txt'  # Replace with the actual path to your word list file
-    words = get_words_from_file(file_path)
+#     file_path = 'sowpods.txt' 
+#     words = get_words_from_file(file_path)
 
-    # Get user input for sentence length
-    try:
-        length = int(input("How long do you want the sentence to be (between 2 and 20)? "))
-        if not (2 <= length <= 20):
-            raise ValueError("Sentence length must be between 2 and 20.")
-    except ValueError as e:
-        print(f"Error: {e}")
-        print("Please enter a valid integer between 2 and 20.")
-        return
+#     try:
+#         length = int(input("How long do you want the sentence to be (between 2 and 20)?"))
+#         if not (2 <= length <= 20):
+#             raise ValueError("Sentence length must be between 2 and 20.")
+#     except ValueError as e:
+#         print(f"Error: {e}")
+#         print("Please enter a valid integer between 2 and 20.")
+#         return
 
-    # Generate and print the random sentence
-    sentence = get_random_sentence(length, words)
-    print("Random Sentence:", sentence)
+#     sentence = get_random_sentence(length, words)
+#     print("Random Sentence:", sentence)
 
-if __name__ == "__main__":
-    main()
-
-
+# if __name__ == "__main__":
+#     main()
 
 # #----------------------------------------------------------------
 # #Exercise 2: Working With JSON 
