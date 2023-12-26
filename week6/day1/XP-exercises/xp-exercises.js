0736bd2839710296daf06b7f695a13762fe4354a
Viktor <korneevv@gmail.com>
2023-12-26 15:29:36 +0200

    w6d1_XP_ex3

--- week6/day1/XP-exercises/xp-exercises.js
+++ week6/day1/XP-exercises/xp-exercises.js
@@ -15,58 +15,62 @@
  
 //   displayNumbersDivisible();  
 
-// Exercise 2 : Shopping List
+// // Exercise 2 : Shopping List
 
-const stock = {
-  "banana": 6,
-  "apple": 0,
-  "pear": 12,
-  "orange": 32,
-  "blueberry": 1
-};
+// const stock = {
+//   "banana": 6,
+//   "apple": 0,
+//   "pear": 12,
+//   "orange": 32,
+//   "blueberry": 1
+// };
 
-const prices = {
-  "banana": 4,
-  "apple": 2,
-  "pear": 1,
-  "orange": 1.5,
-  "blueberry": 10
-};
+// const prices = {
+//   "banana": 4,
+//   "apple": 2,
+//   "pear": 1,
+//   "orange": 1.5,
+//   "blueberry": 10
+// };
 
-const shoppingList = ["banana", "orange", "apple"];
+// const shoppingList = ["banana", "orange", "apple"];
 
-function myBill(shoppingList, stock, prices) {
-  let totalPrice = 0;
+// function myBill(shoppingList, stock, prices) {
+//   let totalPrice = 0;
 
-  for (let i = 0; i < shoppingList.length; i++) {
-    const item = shoppingList[i];
+//   for (let i = 0; i < shoppingList.length; i++) {
+//     const item = shoppingList[i];
     
-    if (item in stock) {
+//     if (item in stock) {
        
-      if (item in prices) {
+//       if (item in prices) {
 
-        if (stock[item] > 0) {
+//         if (stock[item] > 0) {
         
-          totalPrice += stock[item] * prices[item];
+//           totalPrice += stock[item] * prices[item];
          
-          stock[item]--;
+//           stock[item]--;
 
-        } else {
-          console.log(`Attention: ${item} out of the stock.`);
-        }   
+//         } else {
+//           console.log(`Attention: ${item} out of the stock.`);
+//         }   
 
-      } else {
-        console.log(`Attention: No price available for ${item}.`);
-      }
-    } else {
-      console.log(`Attention: ${item} is not in stock.`);
-    }
-  }
+//       } else {
+//         console.log(`Attention: No price available for ${item}.`);
+//       }
+//     } else {
+//       console.log(`Attention: ${item} is not in stock.`);
+//     }
+//   }
 
-  return totalPrice;
-}
+//   return totalPrice;
+// }
 
 
-const total = myBill(shoppingList, stock, prices);
-console.log("Total:", total);
-console.log("Remaining stock:", stock);
+// const total = myBill(shoppingList, stock, prices);
+// console.log("Total:", total);
+// console.log("Remaining stock:", stock);
+
+
+// Exercise 3 : What’s In My Wallet ?
+
