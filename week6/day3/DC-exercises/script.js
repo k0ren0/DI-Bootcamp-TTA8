a313d402091bebb96d2b1e36a48581e5f79ae5bb
Viktor <korneevv@gmail.com>
2023-12-27 20:51:52 +0200

    w6d3_DC_done

--- week6/day3/DC-exercises/script.js
+++ week6/day3/DC-exercises/script.js
@@ -1,12 +1,12 @@
 let client = "John";
 
 const groceries = {
-    fruits : ["pear", "apple", "banana"],
+    fruits: ["pear", "apple", "banana"],
     vegetables: ["tomatoes", "cucumber", "salad"],
-    totalPrice : "20$",
-    other : {
-        paid : true,
-        meansOfPayment : ["cash", "creditCard"]
+    totalPrice: "20$",
+    other: {
+        paid: true,
+        meansOfPayment: ["cash", "creditCard"]
     }
 }
 
@@ -15,32 +15,23 @@ const displayGroceries = () => {
         console.log(fruit);
     });
 };
-displayGroceries();
 
 const cloneGroceries = () => {
     const clonedGroceries = { ...groceries };
-    let user = client;
-    let shopping = groceries;
+    let user = client; // user === John
+    client = "Betty";
+    const shopping = { ...groceries };
+    shopping.totalPrice = "35$";
+    clonedGroceries.other.paid = false;
+
+    console.log("Groceries", groceries);
+    console.log("Shopping", shopping);
+
     return { clonedGroceries, user, shopping };
 }
 
-const { clonedGroceries, user, shopping } = cloneGroceries();
-
-console.log("User:", user);
-
-client = "Betty";
-
-console.log("Modified Client:", client);
-console.log("User after modification:", user);
-
-clonedGroceries.totalPrice = "35$";
-console.log("Modified Cloned Groceries:", clonedGroceries);
-console.log("Shopping after modifying cloned groceries:", shopping); // Yes, we will see the modification in the shopping object. They share the same reference to the original object.
-
-clonedGroceries.other.paid = false;
-console.log("Modified Cloned Groceries:", clonedGroceries);
-console.log("Shopping after modifying cloned groceries:", shopping); 
-
+displayGroceries();
+cloneGroceries();
 
 
 
