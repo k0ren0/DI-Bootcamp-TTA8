f41ccd37ef1097fff20a22b590a3382b439ec8a2
Viktor <korneevv@gmail.com>
2023-12-31 13:40:52 +0200

    w6d5_XP_DC_done

--- week6/day5/DC-exercises/script.js
+++ week6/day5/DC-exercises/script.js
@@ -1,17 +1,17 @@
-// function areAnagrams(str1, str2) {
-//     const cleanStr1 = str1.replace(/\s/g, '').toLowerCase();
-//     const cleanStr2 = str2.replace(/\s/g, '').toLowerCase();
-//     const sortCleanStr1 = cleanStr1.split('').sort().join('');
-//     const sortCleanStr2 = cleanStr2.split('').sort().join('');
-//     return sortCleanStr1 === sortCleanStr2;
-// }
-
-// // OR short variant
-
 function areAnagrams(str1, str2) {
-    const clean = str => str.replace(/\s/g, '').toLowerCase();
-    return clean(str1).split('').sort().join('') === clean(str2).split('').sort().join('');
+    const cleanStr1 = str1.replace(/\s/g, '').toLowerCase();
+    const cleanStr2 = str2.replace(/\s/g, '').toLowerCase();
+    const sortCleanStr1 = cleanStr1.split('').sort().join('');
+    const sortCleanStr2 = cleanStr2.split('').sort().join('');
+    return sortCleanStr1 === sortCleanStr2;
 }
+
+// // // OR short variant
+
+// function areAnagrams(str1, str2) {
+//     const clean = str => str.replace(/\s/g, '').toLowerCase();
+//     return clean(str1).split('').sort().join('') === clean(str2).split('').sort().join('');
+// }
 
 
 // Examples
