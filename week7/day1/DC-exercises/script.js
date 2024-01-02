4acd9f237b8897679d5644c209edcddad1da9fdd
Viktor <korneevv@gmail.com>
2024-01-02 16:33:39 +0200

    w7d1_DC_XP_XP_gold_done

--- week7/day1/DC-exercises/script.js
+++ week7/day1/DC-exercises/script.js
@@ -12,8 +12,25 @@ class Video {
     }
 }
 
-const video1 = new Video('How study in the DI bootcamp', 'John Snow', 13);
+// 3
+const video1 = new Video('How study in the DI bootcamp', 'John Snow', 130);
 video1.watch();
-
-const video2 = new Video("How find first job in Israel's Hi-tech", 'Petrosyan Ivan', 7.40);
+// 4
+const video2 = new Video("How find first job in Israel's Hi-tech", 'Petrosyan Ivan', 180);
 video2.watch();
+
+
+
+// // 5
+// const videosData = [
+//     { title: 'How study in the DI bootcamp', uploader: 'John Snow', time: 130 },
+//     { title: 'ow find first job in Israel's Hi-tech', uploader: 'Petrosyan Ivan', time: 180 },
+//     { title: 'Quickly React', uploader: 'Alice Brake', time: 100 },
+//     { title: 'Node.js Basics', uploader: 'Willy Boy', time: 99 },
+//     { title: 'PHP', uploader: 'Eva Monis', time: 77 }
+// ];
+
+// // 6
+// const videos = videosData.map(data => new Video(data.title, data.uploader, data.time));
+
+// videos.forEach(video => video.watch());
