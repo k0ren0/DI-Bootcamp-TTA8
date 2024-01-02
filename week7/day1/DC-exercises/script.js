e373bdf7d1af059cfe0a08553427261ffc29f897
Viktor <korneevv@gmail.com>
2024-01-02 16:16:57 +0200

    w7d1_DC_XP_XP_gold_done

--- week7/day1/DC-exercises/script.js
+++ week7/day1/DC-exercises/script.js
@@ -1,16 +1,19 @@
-//  Exercise 1 : Location
+// Daily Challenge : Creating Objects
 
-const person = {
-    name: 'John Doe',
-    age: 25,
-    location: {
-        country: 'Canada',
-        city: 'Vancouver',
-        coordinates: [49.2827, -123.1207]
+class Video {
+    constructor(title, uploader, time){
+        this.title = title;
+        this.uploader = uploader;
+        this.time = time;
+    }
+
+    watch() {
+        console.log(`${this.uploader} watched all ${this.time} minutes of ${this.title}!`);
     }
 }
 
-const {name, location: {country, city, coordinates: [lat, lng]}} = person;
+const video1 = new Video('How study in the DI bootcamp', 'John Snow', 13);
+video1.watch();
 
-console.log(`I am ${name} from ${city}, ${country}. Latitude(${lat}), Longitude(${lng})`);
-
+const video2 = new Video("How find first job in Israel's Hi-tech", 'Petrosyan Ivan', 7.40);
+video2.watch();
