6d693288900d94d1aa8461aa44b0615a2adbdd22
Viktor <korneevv@gmail.com>
2024-01-08 21:11:59 +0200

    w8d1_XP_6

---
+++ week8/day1/XP-exercises/xp6/npm-beginner/app.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+const colorfulMessage = chalk.bold.green('Hello, npm beginners!') + ' ' + chalk.bgCyan('Welcome to the world of Node.js and Chalk!');
+
+console.log(colorfulMessage);
