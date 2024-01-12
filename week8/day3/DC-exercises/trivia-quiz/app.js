307e5fcacc7aab3b71d7d3b6877a857fbd412ccd
Viktor <korneevv@gmail.com>
2024-01-12 12:48:11 +0200

    w8d3_DC_done

---
+++ week8/day3/DC-exercises/trivia-quiz/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+const quizRoutes = require("./routes/quizRoutes");
+
+app.use(bodyParser.json());
+app.use('/', quizRoutes);
+
+app.listen(PORT, () => {
+  console.log(`I'm running here http://localhost:${PORT}`);
+});
---
+++ week8/day3/DC-exercises/trivia-quiz/routes/quizRoutes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const triviaQuestions = require("../config/quizModel");
+
+let userScore = 0;
+let currentQuestionIndex = 0;
+
+router.get("/quiz", (req, res) => {
+  if (currentQuestionIndex < triviaQuestions.length) {
+    res.json({ question: triviaQuestions[currentQuestionIndex].question });
+  } else {
+    res.json({ message: 'Quiz is finished. Use /quiz/score to view your score.' });
+  }
+});
+
+router.post("/quiz", (req, res) => {
+  const userAnswer = req.body.answer;
+  const currentQuestion = triviaQuestions[currentQuestionIndex];
+
+  if (userAnswer && userAnswer.toLowerCase() === currentQuestion.answer.toLowerCase()) {
+    userScore++;
+  }
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < triviaQuestions.length) {
+    res.json({ question: triviaQuestions[currentQuestionIndex].question });
+  } else {
+    res.json({ message: 'Quiz finished. Use /quiz/score to view your score.' });
+  }
+});
+
+router.get("/quiz/score", (req, res) => {
+  res.json({ score: userScore });
+});
+
+module.exports = router;
