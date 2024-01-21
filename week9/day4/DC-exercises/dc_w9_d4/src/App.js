01bb1c0238fd9bbc9b63a981b809560f35fa01c1
Viktor <korneevv@gmail.com>
2024-01-21 10:43:24 +0200

    w9d4_dc_done

---
+++ week9/day4/DC-exercises/dc_w9_d4/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [languages, setLanguages] = useState([
+    { name: "Php", votes: 0 },
+    { name: "Python", votes: 0 },
+    { name: "JavaScript", votes: 0 }, 
+    { name: "Java", votes: 0 }
+  ]);
+
+ 
+  const incrementVote = (languageName) => {
+    setLanguages(languages.map(lang => {
+      if (lang.name === languageName) {
+        return { ...lang, votes: lang.votes + 1 };
+      }
+      return lang;
+    }));
+  };
+
+  return (
+    <div>
+      <h1>Vote Your Language!</h1>
+      {languages.map((lang, index) => (
+        <div key={index}>
+          <span>{lang.votes} {lang.name}</span>
+          <button onClick={() => incrementVote(lang.name)}>Click Here</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
