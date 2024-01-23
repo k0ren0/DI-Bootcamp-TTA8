c5d431a0918f3037badd5bab804f52cdc09492f8
Viktor <korneevv@gmail.com>
2024-01-23 10:22:40 +0200

    w10d1_dc_done

--- week10/day2/class/src/App.js
+++ week10/day2/class/src/App.js
@@ -3,17 +3,27 @@ import About from './components/About';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import ErrorPage from './components/ErrorPage';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 /** Routering */
 
 function App() {
   return (
     <div className="App">
+      <header> 
+        <nav>
+          <Link to="/">Home</Link>{" "}
+          <Link to="/about">About</Link>{" "}
+          <Link to="/shop">Shop</Link>{" "}
+        </nav>
+      </header>
+
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/shop" element={<Shop />}/>
+        <Route path="/shop/:productid" element={<Shop />}/>
+        <Route path="/shop/:productid/:shopid" element={<Shop />}/>
         <Route path="/*" element={<ErrorPage />}/>
       </Routes>
 
--- week10/day2/class/src/components/Shop.js
+++ week10/day2/class/src/components/Shop.js
@@ -1,5 +1,14 @@
+import { useParams } from "react-router-dom";
+
 const Shop = (props) => {
-    return <h1>Shop</h1>
-}
+    const params = useParams();
+    console.log(params);
+    return (
+        <>
+            <h1>Shop</h1>
+            <h2>Product: {params.productid}</h2>
+        </>
+    );
+};
 
 export default Shop;
