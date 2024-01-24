943e936b4201603b115f62769b45a6afca4a60c3
Viktor <korneevv@gmail.com>
2024-01-24 17:07:51 +0200

    w10d3_xp2_done

---
+++ week10/day3/XP-exercises/xp2_w10d3/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useRef } from "react";
+import "./App.css";
+
+function CharacterCounter() {
+  const inputRef = useRef(null);
+  const [charCount, setCharCount] = useState(0);
+
+  const handleInputChange = () => {
+    const textLength = inputRef.current.value.length;
+    setCharCount(textLength); // Update the character count based on textLength
+  };
+
+  return (
+    <div>
+      <h1>Character Counter</h1>
+      <textarea
+        ref={inputRef}
+        onChange={handleInputChange}
+        placeholder="Type something..."></textarea>
+      <p>Character Count: {charCount}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <CharacterCounter />
+    </div>
+  );
+}
+
+export default App;
