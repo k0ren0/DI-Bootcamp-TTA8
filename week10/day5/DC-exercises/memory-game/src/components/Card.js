16eeb26902eb642adb3338faf0b078fb7f1cfa65
Viktor <korneevv@gmail.com>
2024-01-25 18:01:28 +0200

    w10d5_dc_done

--- week10/day5/DC-exercises/memory-game/src/components/Card.js
+++ week10/day5/DC-exercises/memory-game/src/components/Card.js
@@ -6,8 +6,8 @@ const Card = ({ hero, onCardClick }) => {
     <div className="card" onClick={() => onCardClick(hero)}>
       <img src={hero.image} alt={hero.name} />
       <div className="card-info">
-        <h3>Name: {hero.name}</h3>
-        <p>Occupation: {hero.occupation}</p>
+        <h3><strong>Name:</strong>{hero.name}</h3>
+        <p><strong>Occupation:</strong> {hero.occupation}</p>
       </div>
     </div>
   );
