2f6372cf1559801b021180f09fdde53a2b740c9b
Viktor <korneevv@gmail.com>
2024-01-27 11:05:01 +0200

    w10d5_xp_final_done

--- week10/day5/XP-exercises/mp-searchbook-app-w10d5/src/components/BookList.js
+++ week10/day5/XP-exercises/mp-searchbook-app-w10d5/src/components/BookList.js
@@ -9,43 +9,41 @@ const BookList = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [sortOrder, setSortOrder] = useState('newest');
   const [error, setError] = useState(null);
+  const [isSearching, setIsSearching] = useState(false);
 
-  // Define the fetchBooks function
   const fetchBooks = useCallback(async () => {
-    if (!searchTerm) return; // Don't make a request if searchTerm is empty
-  
-    setError(null); // Reset error before making a request
+    if (!searchTerm) return;
+
+    setError(null);
     try {
       const result = await axios(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`);
       setBooks(result.data.items || []);
     } catch (error) {
       if (error.response) {
-        // The request was made, but the server responded with an error status code
         setError(`Error: ${error.response.status} - ${error.response.statusText}`);
         console.error('Error response:', error.response.data);
       } else if (error.request) {
-        // The request was made, but no response was received
         setError('No response received from the server');
         console.error('No response received:', error.request);
       } else {
-        // Something happened in setting up the request
         setError('Error setting up the request');
         console.error('Request setup error:', error.message);
       }
+    } finally {
+      setIsSearching(false);
     }
   }, [searchTerm]);
-  
 
   useEffect(() => {
-    fetchBooks(); // Initial request when the component mounts
-  }, [searchTerm, fetchBooks]);
+    if (isSearching) {
+      fetchBooks();
+    }
+  }, [isSearching, fetchBooks]);
 
-  // Function to handle search
   const onSearch = () => {
-    fetchBooks(); // Trigger a request based on the searchTerm
+    setIsSearching(true);
   };
 
-  // Function to sort books
   const sortBooks = (books, sortOrder) => {
     return books.sort((a, b) => {
       if (sortOrder === 'newest') {
@@ -60,6 +58,7 @@ const BookList = () => {
     <div>
       <div className="row justify-content-center align-items-center mb-3">
         <div className="col-md-3">
+          {/* Обновленный SearchBox с передачей функции onSearch */}
           <SearchBox setSearchTerm={setSearchTerm} onSearch={onSearch} />
         </div>
         <div className="col-md-3">
