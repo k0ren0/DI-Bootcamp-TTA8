18b2b5185830ad73babf880c3b7cbdc64631168f
Viktor <korneevv@gmail.com>
2024-01-30 17:47:22 +0200

    w11d2_dc_done

--- week11/day2/DC-exercises/dc-w11d2/src/app/store.js
+++ week11/day2/DC-exercises/dc-w11d2/src/app/store.js
@@ -1,11 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import ageReducer from '../features/age/ageSlice';
+import * as thunk from 'redux-thunk';
 
 
 const store = configureStore({
   reducer: {
     age: ageReducer,
   },
+  // middleware: [...getDefaultMiddleware(), thunk],
 });
 
 export default store;
